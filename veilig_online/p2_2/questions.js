03bcd00765afc43f6053d89c48918a3a5da0fea4
Spoon404 <36400163+404spoon404@users.noreply.github.com>
2022-11-07 14:23:58 +0100

    Lesson 2.2 finished

--- veilig_online/p2_2/questions.js
+++ veilig_online/p2_2/questions.js
@@ -3,115 +3,96 @@ const questions =
 // This file hosts all of the question pages (HTML) and questions
 [
 	{
-		"header": "Hoe werkt het internet?", // Can be undefined for no header
+		"header": "Hoe volgen ze je?", // Can be undefined for no header
 		"content": [ // Contains the paragraphs (the <p> elements), can be an empty array for no content
-			"Je weet al best wel wat van veiligheid online. Als je veel weet kan je ermee gaan spelen. Dit is ook wat hackers doen. Ze zorgen dat ze zoveel mogelijk weten over een onderwerp en gaan dan kijken hoe ze het kunnen breken. Voor hackers is de wereld een grote puzzel. Je moet alleen wel de puzzelstukjes hebben voor je ermee aan de slag gaat.",
-			"Deze les gaan we de puzzelstukjes bekijken van de werking van het internet. De volgende les gaan we kijken wat we daar verder mee kunnen",
-			"Het diagram hieronder is een kort overzicht van hoe het internet werkt. Dit bespreken we verder in de volgende pagina's",
-			"<img>imagelarge,./IMG/Overzicht_netwerken.png,Een totaaloverzicht van het netwerk"
+			"In 2.1 hebben we gekeken hoe het bezoeken van een website werkt. We hebben de verschillende onderdelen van een verbinding bekeken en weten nu dat als wij een website bezoeken wij deze naar onze eigen computer downloaden.",
+			"Deze les gaan we kijken hoe we dan gevolgd worden en wat we daaraan kunnen doen. De twee onderwerpen zijn Cookies en Tracking content. Als bonus hebben wij het over een DDOS.",
+			"Eerst even als herinnering het schema van vorige les.",
+			"<img>imagemidl,./IMG/overzicht_netwerken.png,Een totaaloverzicht van het netwerk"
 		],
 		"question": undefined, // Documentation on question is on the next item
 		"next": true // Adds the next button ('buttonforward')
 	},
 	{
-		"header": "Begrippen",
+		"header": "Cookies",
 		"content": [
-			"Hiero",
-			"<img>imagesmallr,./IMG/Eigen_computer_inside.png,Computer icon",
-			"<t>Computer",
-			"Dit is je eigen computer. De rode rand er om heen is de Firewall. Deze zorgt ervoor dat andere machines niet zomaar op je computer kunnen kijken.",
-			"Onder de zwarte streep staat je opslag. Dat is dus je harde schijf.",
-			"Je ziet dat er een rood vakje om je browser heen staat. Dit is de Sandbox (zandbak). Je browser kan wel dingen op je harde schijf opslaan en zelfs programma's op websites draaien maar krijgt een eigen stukje computer dat niet met de rest van je computer kan praten. Dit zorgt ervoor dat een website niet zomaar je hele harde schijf kan wissen.",
-			"<img>imagevsmallr,./IMG/Browser.png,Browser icon",
-			"<t>Browser",
-			"Dit is het programma dat je gebruikt om het Internet op te gaan. Dit kan Edge, Chrome, Firefox, Safari of een ander programma zijn.",
-			"<img>imagevsmallr,./IMG/Modem_firewall.png,Modem en firewall icon",
-			"<t>Modem en Firewall",
-			"Tussen je thuisnetwerk zitten je modem en in het modem zit een firewall. Het modem is het apparaat dat verbinding maakt met je provider. Dit kan met Coax (tv kabel), ADSL (telefoonlijn) of Glasvezel werken.",
-			"De firewall zorgt ervoor dat de rest van het Internet niet zomaar in je netwerk kan kijken. Deze firewall houdt alle vragen van buiten je eigen netwerk tegen. Niemand kan in je netwerk kijken zonder deze Firewall te hacken.",
-			"<img>imagevsmallr,./IMG/DNS_server.png,een computer",
-			"<t>DNS Server",
-			"Een DNS (Domain Name System) server is een computer op het internet die de namen van websites vertaald naar de bijbehorende IP (Internet Protocol) adressen. Het IP adres kan je zien als de postcode en het huisnummer van een internetverbinding.",
-			"<img>imagesmallr,./IMG/Datacentrum.png,een computer",
-			"<t>Datacentrum",
-			"Een datacentrum is een gebouw waar heel veel computers staan. In dit voorbeeld is het een datacentrum waar verschillende websites gehost worden. Het hosten van een website is het aanbieden van de site op het internet.",
-			"<img>imagevsmallr,./IMG/Server.png,een computer",
-			"<t>Server",
-			"Een server is een computer die bedoeld is om informatie aan andere computers te geven. Bij een webserver is de informatie de website. Microsoft Teams heeft ook servers deze servers bewaren de gegevens in Teams en zorgen dat de gebruikers met elkaar kunnen praten.",
-			"<img>imagevsmallr,./IMG/Webserver.png,een computer",
-			"<t>Webserver",
-			"Een server met als enige doel het aanbieden van een of meer websites."
+			"Hieronder staat het schema van de vorige les maar dan iets verder uitgezoomd. In plaats van 1 datacentrum hebben we er nu 3. Deze gaan we in de rest van de uitleg gebruiken",
+			"<img>imagemidl,./IMG/totaal_leeg.jpg,",
+			"<t>Login cookie",
+			"Hoe werkt een login cookie? Als je voor het eerst een website bezoekt ontvang je de eerste pagina en kan je inloggen.",
+			"<img>imagemidl,./IMG/website_bezoek.jpg,",
+			"Als je dan je login gegevens hebt ingevuld en je drukt op login. Worden deze gegevens naar de computer waar de website op staat gestuurd. De computer controleert dit en maakt een cookie. Dit cookie is een soort toegangskaartje. Er staat een code op die ervoor zorgt dat je alleen met dit cookie toegang krijgt.",
+			"De site stuurt dit cookie terug naar je computer en je browser bewaart dit.",
+			"<img>imagemidl,./IMG/wbsite_login.jpg,",
+			"Als je nu ergens op de website klikt stuurt je computer niet alleen waar je op hebt geklikt maar ook het cookie naar de website. De server controleert dit en stuurt je de volgende pagina.",
+			"<img>imagemidl,./IMG/website_login_cookie.jpg,",
+			"<t>Analytische cookies",
+			"Cookies worden niet alleen gebruikt voor inloggen. De eigenaar van een website wil vaak weten wat je op de website doet. Hoelang kijk je naar een pagina, zoom je in en wat doe je verder. Hiervoor gebruiken ze analytische cookies.",
+			"Als je klikt op een link op de site stuur je een verzoek naar de server, vaak dat je een andere pagina wil zien. De server zet de pagina klaar en maakt een cookie waarin staat hoe je de vorige pagina gebruikt hebt. Dit cookie gaat mee met de nieuwe pagina en je browser slaat dit op.",
+			"<img>imagemidl,./IMG/website_analytisch_cookie.jpg,",
+			"Als je nu op een andere link klikt stuurt je browser niet alleen de klik maar ook het vorige analytische cookie naar de server. Zo bewaar je zelf wat je allemaal op de site gedaan hebt. Als de eigenaar de analytische cookies bekijkt kunnen ze precies zien wat je allemaal gedaan hebt.",
+			"<img>imagemidl,./IMG/website_analytisch_cookie2.jpg,",
 		],
 		"question": {
-			"ask": "Een browser kan zelf programma's op je computer installeren.", // The question
+			"ask": "Functionele cookies zijn nodig om te kunnen inloggen.", // The question
 			"answers": [ // The answers available to give
-				"Dat is waar, maar alleen in de Sandbox",
-				"Dat is waar",
-				"Dat is niet waar",
+				"Dat is waar, je gebruikersnaam en wachtwoord staan erin",
+				"Dat is waar, het is je toegangsbewijs",
+				"Dat is niet waar, de server kan mijn IP onthouden",
 			],
-			"correct": 2 // Index in "answers" of the correct answer (answers[2] is "Weggooien" in this case)
+			"correct": 1 // Index in "answers" of the correct answer (answers[2] is "Weggooien" in this case)
 		},
 		"next": true
 	},
 	{
-		"header": "Netwerken",
+		"header": "Tracking Content",
 		"content": [
-			"Tekst",
-			"<img>imagelarge,./IMG/Overzicht_netwerken.png,Een totaaloverzicht van het netwerk",
-			"Dit is een overzicht van het netwerk dat je nodig hebt om een verbinding met een website te maken. In werkelijkheid staat er natuurlijk nog veel meer op het internet. Om het totaal van het internet in te schatten mag je de rechterkant van het plaatje  ongeveer 4 miljard herhalen. Dat is het aantal IP adressen dat we nu kunnen gebruiken.",
-			"Dit is al te weining dus we zijn met een nieuw soort IP adressen bezig. Daarmee kunnen 340.282.366.920.938.000.000.000.000.000.000.000.000 apparaten op het internet aansluiten.",
-			"<t> Interne Netwerk",
-			"<img>imagesmall,./IMG/Netwerk_binnen.png,Ingezoomd op netwerk binnen",
-			"Dit is een voorbeeld van je eigen thuisnetwerk met je eigen computer uitvergroot.",
-			"Elk apparaat in je netwerk heeft een eigen IP adres. Dit gebruikt je modem om de juiste informatie naar de goede computer te sturen. De IP adressen in je eigen netwerk beginnen met 192, 172 of 10. Dit hebben wij zo afgesproken. Het is aan de linkerkant van de Firewall. Het grote boze internet ziet deze dus niet. Het internet weet dus ook niet welk apparaat in je netwerk een site heeft bezocht. Je modem onthoud welk apparaat iets aan het internet gevraagd heeft en stuurt het naar de goede computer.",
-			"<t> Externe netwerk",
-			"<img>imagesmall,./IMG/Netwerk_buiten.png,Ingezoomd op netwerk buiten",
-			"Dit is het grote boze internet. De verschillende aansluitingen hebben allemaal een eigen IP adres. Als je een website bezoekt heb je dus het IP adres van die aansluiting nodig.",
-			"Je eigen huis heeft hier dus 1 eigen IP adres. Dit ziet elke website die je bezoekt."
+			"De functionele cookies zijn nodig om het internet goed te laten werken. Analytische cookies zijn handig voor de makers van websites. Er zijn ook cookies die alleen maar gemaakt zijn om je te volgen op het internet. Dit zijn tracking cookies.",
+			"Als je een pagina bezoekt en je klikt gaat er weer een verzoek naar de website. Deze maakt een tracking cookie. Je ontvangt nu de pagina en een nieuw tracking cookie.",
+			"<img>imagemidl,./IMG/website_tracking_cookie1.jpg,",
+			"Bij de volgende klik gaat je tracking cookie mee. De server weet nu dat je de volgende pagina wil hebben maar ze weten ook wat je hiervoor op de pagina gedaan hebt. Ze kunnen het tracking cookie naar een advertentie server sturen. Hier start een veiling met jouw gegevens. En je krijgt de advertentie op basis van de dingen die je al op site gedaan hebt.",
+			"<img>imagemidl,./IMG/website_tracking_cookie2.jpg,",
+			"Het is niet zo erg als dit gebeurt op basis van maar één website. De meeste websites weten niet zo heel veel van ons. Adverteerders willen alleen graag nog meer informatie van je hebben. En daar hebben ze iets voor verzonnen.",
+			"<img>imagemidl,./IMG/website_tracking_cookie3.jpg,",
+			"Als je een website bezoekt stuur je de tracking cookie naar de site die je bezoekt. Als er op die site iets staat dat van een andere server komt (bijvoorbeeld een dit vind ik leuk van Facebook) kan het tracking cookie ook naar die andere server gestuurd worden.",
+			"Deze servers bewaren ook gegevens uit oude tracking cookies. Andere websites die je eerder bezocht hebt. Nu hebben ze informatie van nog meer dan één website. Ze weten dus precies wat je allemaal gedaan hebt. Van die informatie maken ze een profiel. Met dit profiel sturen ze je een advertentie die precies bij je past.",
+			"<img>imagemidl,./IMG/website_tracking_cookie4.jpg,",
+			"",
+			"",
 		],
 		"question": {
-			"ask": "Een website kan zien vanaf welke computer je een website bezoekt",
-			"answers": ["Dat is waar anders kan je de site niet zien", "Dat is niet waar, de site ziet alleen maar het IP van je huis", "Dat is niet waar, de firewall zit ertussen"],
+			"ask": "In een analytisch cookie staan alle sites die je bezocht hebt",
+			"answers": [
+			"Dat is waar, anders kunnen ze je niet volgen",
+			"Dat is niet waar, een cookie kan alleen praten met de site waar het vandaan komt",
+			"Dat is niet waar, een cookie kan geen adressen bewaren",
+			"Dat is waar, anders ontvang je veel teveel cookies",
+			],
 			"correct": 1
 		},
 		"next": true
 	},
 	{
-		"header": "Een website bezoeken 1",
+		"header": "DDOS",
 		"content": [
-			"Tekst",
-			"<t> Begin verbinding DNS",
-			"<img>imagemid,./IMG/DNS_intern.png,Een DNS request in netwerk",
-			"De DNS aanvraag komt als eerste bij je modem. Je modem heeft ook een kleine database voor adressen. Websites die je veel bezoekt worden daar opgeslagen. In dit geval had je google.com al eerder opgezocht en geeft je router als antwoord 172.217.168.206. Je browser weet nu waar Google.com is.",
-			"<t> Extern verbinding DNS",
-			"<img>imagemid,./IMG/DNS_extern.png,Een DNS request op internet",
-			"Als je nog niet eerder, of lang geleden bij Google geweest was weet je modem niet waar de website is. De machine stuurt je vraag dan door naar een DNS server. De eerste DNS server staat vaak bij je provider. Als die het antwoord ook niet weet wordt de vraag verder doorgestuurd naar DNS servers die meer adressen kennen. Dit gaat net zolang door tot je antwoord krijgt. Als het te lang duurt voor het antwoord terug komt geeft je browser aan dat de website niet gevonden kan worden.",
-			"In dit voorbeeld weet de eerste DNS server het goede antwoord en geeft die het door aan je modem die het weer aan je computer verteld."
+			"We hebben het in deze lessen over online veiligheid. Ik heb gemerkt dat veel van mijn leerlingen wel last hebben gehad van een DDOS aanval. Daarom hier de bonus DDOS uitleg.",
+			"Een DDOS is een Distributed Denial Of Service. In het Nederlands een verdeelde onderbreking van een dienst. Dat helpt nog niet echt met het begrijpen.",
+			"Bij een DDOS vallen allemaal verschillende computers uit heel de wereld één adres aan. Door met zijn allen tegelijk naar een adres te gaan kan de computer aan de andere kant de drukte niet meer aan en valt het uit. Een DDOS is eigenlijk een file op het internet.",
+			"Om een DDOS uit te voeren heb je een CnC (Command and Control) server nodig en verschillende gehackte computers. De gehackte computers noemen we ook wel bots of zombies. Hierom is het verboden om een DDOS te doen. Je gebruikt gehackte computers van andere mensen. Dat mag niet.",
+			"De eigenaar van de DDOS zet in de CnC server wat het IP-adres is van de verbinding die aangevallen moet worden. Deze server stuurt de opdracht naar alle zombies.",
+			"<img>imagemidl,./IMG/DDOS1.jpg,",
+			"De zombies gaan allemaal proberen het adres te bezoeken. In het plaatje zijn het er drie. In de werkelijkheid zijn dit duizenden computers tegelijk. Als jouw verbinding thuis het doel is stopt je modem de aanval. Je internet is wel heel traag of valt uit maar ze komen je netwerk niet binnen.",
+			"<img>imagemidl,./IMG/DDOS2.jpg,",
+			"Voor iemand met een website of een onlinedienst gebeurd er iets anders. Als de DDOS gericht is op de website laat het modem de aanval netjes door. Het modem kan niet ziet wat echte aanvragen zijn en wat niet. De webserver krijgt het nu heel druk. Als de verbinding heel goed is kan de server zelfs oververhitten. Niemand kan de website meer bezoeken en de dienst(de website) is onderbroken.",
+			"<img>imagemidl,./IMG/DDOS3.jpg,",
 		],
 		"question": {
-			"ask": "Iemand die op het modem kan, kan zien welke sites je bezocht hebt",
-			"answers": ["Dat is waar, het modem onthoud dit", "Dat is niet waar, het modem onthoud dit niet", "Dat is alleen waar als je DNS gebruikt", "Dat is waar, Het modem controleert alle verbindingen"],
-			"correct": 0
-		},
-		"next": true
-	},
-	{
-		"header": "Een website bezoeken 2",
-		"content": [
-			"Als je browser weet waar een site staat kan je deze bezoeken. De DNS aanvraag begint meteen nadat je een site aanklikt of op enter drukt. Daarna start het bezoeken van de site automatisch. Dit gaat dus heel snel.",
-			"<t>De site bereiken",
-			"<img>imagemid,./IMG/Site_load_overzicht.png,Het bezoeken van een webserver",
-			"De browser stuurt een verzoek voor de site naar je modem. Deze onthoud welke computer dit gevraagd heeft en stuurt het door naar het grote boze Internet. Het Internet stuurt dit verzoek naar de computer met het IP adres van de website vaak is dit een datacentrum.",
-			"De ontvanger heeft ook een modem en een firewall. Ze hebben alleen een gaatje in de firewall gemaakt. In het modem staat een regel. Als iemand de site wil bekijken mag dat.",
-			"Het modem stuurt de vraag om de website naar de goede computer in het datacentrum die daarmee aan het werk gaat.",
-			"<t>De site downloaden",
-			"<img>imagemid,./IMG/Site_reply_overzicht.png,Het antwoord van de webserver",
-			"Tekst"
-		],
-		"question": {
-			"ask": "Een website dit je bezocht hebt staat op je harde schijf",
-			"answers": ["Dat is waar, maar na een tijdje vergeet hij dit", "Dat is niet waar, de site staat op het Internet"],
-			"correct": 0
+			"ask": "Je mag als grap een DDOS op iemand laten doen",
+			"answers": ["Dat is waar, als ik toestemming heb",
+			"Dat is niet waar, omdat je gehackte computers moet hebben",
+			"Dat is waar, een DDOS maakt niets kapot",
+			"Dat is niet waar, je mag niet hacken"],
+			"correct": 1
 		},
 		"next": true
 	},
